59a38c67da8b0609d95a3179daff18d7fd4eb46c
Kohki-Amemiya <amemiya.kohki.q2@dc.tohoku.ac.jp>
2024-08-17 16:31:30 +0900

    modify hpdf

--- learn/code/comp_acc.py
+++ learn/code/comp_acc.py
@@ -4,21 +4,34 @@ import matplotlib.pyplot as plt
 # CSVファイルを読み込む
 df = pd.read_csv('../csv/accuracy.csv')
 
+# フォントとスタイルの設定
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams["font.size"] = 15
+plt.rcParams['axes.grid'] = True
+plt.rcParams["legend.fancybox"] = False
+plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["legend.edgecolor"] = 'black'
+plt.rcParams["legend.handlelength"] = 1
+plt.rcParams["legend.labelspacing"] = 0.5
+plt.rcParams["legend.handletextpad"] = 0.5
+plt.rcParams["legend.markerscale"] = 2
+
 # グラフを作成する
 plt.figure(figsize=(10, 6))
 
-# ML accuracyをプロットする
-plt.plot(df['layers'], df['ML_acc'], label='Machine Learning', marker='o')
+# ML accuracyをプロットする（誤差バー付き）
+plt.errorbar(df['layers'], df['ML_acc'], yerr=df['ML_err'], label='Machine Learning', fmt='o', capsize=5, linestyle='-', markerfacecolor='white', markeredgewidth=1)
 
-# Likelihood accuracyをプロットする
-plt.plot(df['layers'], df['Likekihood_acc'], label='Likelihood', marker='x')
+# Likelihood accuracyをプロットする（誤差バー付き）
+plt.errorbar(df['layers'], df['Likelihood_acc'], yerr=df['Likelihood_err'], label='Likelihood', fmt='^', capsize=5, linestyle='--', markerfacecolor='white', markeredgewidth=1)
 
 # グラフのタイトルとラベルを設定する
-plt.title('Accuracy transition', fontsize=20)
+plt.title('Accuracy Transition', fontsize=20, fontweight='bold')
 plt.xlabel('Number of Layers', fontsize=16)
 plt.ylabel('Accuracy', fontsize=16)
 
-plt.ylim(0.8, 1.00)
+plt.ylim(0.9, 1.00)
 
 # 軸の文字サイズを設定し、小数点2桁で表示する
 plt.xticks(fontsize=12)
@@ -30,5 +43,8 @@ plt.legend(loc='lower right', fontsize=14)
 # グリッドを表示する
 plt.grid(True)
 
-# グラフを表示する
-plt.savefig('../figures/comp_acc.png')
+# グラフを保存する
+plt.savefig('../figures/comp_acc.png', bbox_inches="tight", pad_inches=0.05)
+
+# # グラフを表示する
+# plt.show()
